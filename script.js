8565c3c8cc35557ab788b013243f4ba4fbd79323
panhusa <gas.dawid@gmail.com>
2024-06-01 11:31:44 +0200

    youtube i media

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-
-        // function adjustPlayerHeight() {
-        //     const bioElement = document.querySelector('.bio');
-        //     const playerElement = document.getElementById('spotify-player').parentElement;
-        //     const bioHeight = bioElement.clientHeight;
-
-        //     if (window.innerWidth <= 767) {
-        //         playerElement.style.height = 'auto';
-        //         document.getElementById('spotify-player').style.height = 'auto';
-        //     } else {
-        //         const newHeight = bioHeight * 1.4;
-        //         playerElement.style.height = newHeight + 'px';
-        //         document.getElementById('spotify-player').style.height = newHeight + 'px';
-        //     }
-        // }
-
-        // window.addEventListener('resize', adjustPlayerHeight);
-        // window.addEventListener('load', adjustPlayerHeight);
- 
+// player spotify
         function adjustPlayerHeight() {
             const bioElement = document.querySelector('.bio');
             const playerElement = document.getElementById('player');
@@ -42,3 +24,31 @@
 
         window.addEventListener('resize', adjustPlayerHeight);
         window.addEventListener('load', adjustPlayerHeight);
+
+// youtube container
+        function adjustHeights() {
+            const youtubeContainer = document.querySelector('.youtube-container');
+            const bckElement = document.querySelector('.bck');
+            const youtubeIframe = document.querySelector('.youtube');
+
+            // Get the height of the youtube container
+            const youtubeContainerHeight = youtubeContainer.clientHeight;
+
+            // Calculate the height for the iframe to maintain the 16:9 aspect ratio
+            const iframeWidth = youtubeContainer.clientWidth;
+            const iframeHeight = iframeWidth * 9 / 16;
+
+            // Set the height of the iframe
+            youtubeIframe.style.height = iframeHeight + 'px';
+
+            // Set the height of bck so that part of the iframe fits within it
+            const bckHeight = iframeHeight - youtubeContainerHeight;
+            bckElement.style.height = bckHeight + 'px';
+        }
+
+        // Adjust the height on window resize and load
+        window.addEventListener('resize', adjustHeights);
+        window.addEventListener('load', adjustHeights);
+
+        // Initial call to set the height on page load
+        adjustHeights();
