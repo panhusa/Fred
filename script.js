3297911cc676d8ff3051931b07638f3f9852c814
panhusa <gas.dawid@gmail.com>
2024-07-23 22:26:15 +0200

    progress bar added

--- script.js
+++ script.js
@@ -51,3 +51,13 @@ window.addEventListener('load', adjustHeights);
 adjustHeights();
 
 // youtubeContainer END!!!!!!!!!!!
+
+
+window.onscroll = function () { updateProgressBar() };
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
